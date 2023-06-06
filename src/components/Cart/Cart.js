cf743ebd1959a5e19af53001eae55bc9932c3a71
talhatahir <trinitro619@gmail.com>
2023-06-06 11:33:59 +0500

    Remove React Import statement

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,38 +1,43 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import CartItem from './CartItem';
+import PropTypes from "prop-types";
+import CartItem from "./CartItem";
 
 const Cart = ({ items, total, currency, removeFromCart }) => {
-    return (
-        <div>
-            <h3>Shopping Cart</h3>
+  return (
+    <div>
+      <h3>Shopping Cart</h3>
 
-            <div className="cart">
-                <div className="panel panel-default">
-                    <div className="panel-body">
-                        {items.length > 0 && (
-                            <div className="cart__body">
-                                {items.map(item => (
-                                    <CartItem key={item.id} {...item} onClick={() => removeFromCart(item.id)} />
-                                ))}
-                            </div>
-                        )}
-                        {items.length === 0 && (
-                            <div className="alert alert-info">Cart is empty</div>
-                        )}
-                        <div className="cart__total">Total: {total} {currency}</div>
-                    </div>
-                </div>
+      <div className="cart">
+        <div className="panel panel-default">
+          <div className="panel-body">
+            {items.length > 0 && (
+              <div className="cart__body">
+                {items.map((item) => (
+                  <CartItem
+                    key={item.id}
+                    {...item}
+                    onClick={() => removeFromCart(item.id)}
+                  />
+                ))}
+              </div>
+            )}
+            {items.length === 0 && (
+              <div className="alert alert-info">Cart is empty</div>
+            )}
+            <div className="cart__total">
+              Total: {total} {currency}
             </div>
+          </div>
         </div>
-    );
-}
+      </div>
+    </div>
+  );
+};
 
 Cart.propTypes = {
-    items: PropTypes.array,
-    total: PropTypes.number,
-    currency: PropTypes.string,
-    removeFromCart: PropTypes.func.isRequired
-}
+  items: PropTypes.array,
+  total: PropTypes.number,
+  currency: PropTypes.string,
+  removeFromCart: PropTypes.func.isRequired,
+};
 
 export default Cart;
--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,49 +1,51 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+import { Component } from "react";
+import PropTypes from "prop-types";
 
 class Product extends Component {
-    handleClick = () => {
-        const { id, addToCart, removeFromCart, isInCart } = this.props;
+  handleClick = () => {
+    const { id, addToCart, removeFromCart, isInCart } = this.props;
 
-        if (isInCart) {
-            removeFromCart(id);
-        } else {
-            addToCart(id);
-        }
+    if (isInCart) {
+      removeFromCart(id);
+    } else {
+      addToCart(id);
     }
+  };
 
-    render() {
-        const { name, price, currency, image, isInCart } = this.props;
+  render() {
+    const { name, price, currency, image, isInCart } = this.props;
 
-        return (
-            <div className="product thumbnail">
-                <img src={image} alt="product" />
-                <div className="caption">
-                    <h3>{name}</h3>
-                    <div className="product__price">{price} {currency}</div>
-                    <div className="product__button-wrap">
-                        <button
-                            className={isInCart ? 'btn btn-danger' : 'btn btn-primary'}
-                            onClick={this.handleClick}
-                        >
-                            {isInCart ? 'Remove' : 'Add to cart'}
-                        </button>
-                    </div>
-                </div>
-            </div>
-        );
-    }
+    return (
+      <div className="product thumbnail">
+        <img src={image} alt="product" />
+        <div className="caption">
+          <h3>{name}</h3>
+          <div className="product__price">
+            {price} {currency}
+          </div>
+          <div className="product__button-wrap">
+            <button
+              className={isInCart ? "btn btn-danger" : "btn btn-primary"}
+              onClick={this.handleClick}
+            >
+              {isInCart ? "Remove" : "Add to cart"}
+            </button>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 Product.propTypes = {
-    id: PropTypes.number.isRequired,
-    name: PropTypes.string.isRequired,
-    price: PropTypes.number,
-    currency: PropTypes.string,
-    image: PropTypes.string,
-    isInCart: PropTypes.bool.isRequired,
-    addToCart: PropTypes.func.isRequired,
-    removeFromCart: PropTypes.func.isRequired,
-}
+  id: PropTypes.number.isRequired,
+  name: PropTypes.string.isRequired,
+  price: PropTypes.number,
+  currency: PropTypes.string,
+  image: PropTypes.string,
+  isInCart: PropTypes.bool.isRequired,
+  addToCart: PropTypes.func.isRequired,
+  removeFromCart: PropTypes.func.isRequired,
+};
 
 export default Product;
